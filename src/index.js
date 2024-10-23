0aa03af8e95be5ad179165123b8771f87d328c75
Bernice Templeman <bernice.templeman1@gmail.com>
2024-10-23 13:02:05 -0400

    fix: index.js reverse string for web450-1.1 bernicetempleman1

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@
  * Author: Professor Krasso
  * Date: 13 September 2024
  * File: index.js
- * Description: This script is the main entry point for the
+ * Description: This script is the main entry point for the utils
  */
 
 // Import the factorial function from the factorial.js file
 const { factorial } = require('./utils/factorial');
+
+// Import the reverseString function from the reverseStringjs file
 const { reverseString } = require('./utils/reverseString');
 
 // The main() function calculates the factorial of 5 and logs the result to the console
+// and reverse  the string "evil" and logs the result to the console.
 async function main () {
   const result = factorial(5); // Call the factorial function with the value of 5
   console.log('factorial of 5 is', result); // Log the result to the console
@@ -18,4 +21,4 @@ async function main () {
   console.log('the reversed string of "evil" is: ', reversedStr); // Log the result to the console
 }
 
-main(); // Call the main function to calculate the factorial of 5
+main(); // Call the main function to calculate the factorial of 5 and reverse the string "evil"
